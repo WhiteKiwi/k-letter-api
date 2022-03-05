74e905430b29f01df4b2ba0ae3da2b8a5fbf28db
장지훈 <jh145478@gmail.com>
2022-03-05 14:09:19 +0900

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { Env } from '@config/env';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+	const app = await NestFactory.create(AppModule);
+
+	const configService: ConfigService<Env> = app.get(ConfigService);
+	const port = configService.get<number>('PORT') || 3000;
+
+	await app.listen(port, () => {
+		console.log(`SERVER LISTENING ON port ${port}`);
+	});
+}
+bootstrap();
