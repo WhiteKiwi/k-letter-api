f1c3c601a1522d579166d803402cb7e5b92ec56f
장지훈 <kiwi.jang@toss.im>
2022-03-05 23:58:05 +0900

    feat(): Swagger 적용

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { Env } from '@config/env';
+import { setupSwagger } from '@core/setup';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
@@ -7,9 +8,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
+	setupSwagger(app);
+
 	const configService: ConfigService<Env> = app.get(ConfigService);
 	const port = configService.get<number>('PORT') || 3000;
-
 	await app.listen(port, () => {
 		console.log(`SERVER LISTENING ON port ${port}`);
 	});
